c7511094c2d1f05a70f9126bc5eb0067d2e1b221
1gone <soba4ka808@gmail.com>
2023-03-09 17:17:52 +0700

    lab3-8

---
+++ lab3/8.cpp
@@ -0,0 +1,34 @@
+/*Вариант 8
+Дан текстовый файл. Переписать из исходного файла 
+в другой файл все строки не длиннее n символов.*/
+#include <iostream>
+using namespace std;
+#include <fstream>
+#include <iomanip>
+#include <locale.h>
+#include <string.h>
+
+int main(void){
+    setlocale(LC_ALL,"Rus");
+    ifstream is("text.txt");
+    ofstream os("output.txt");
+    if (!is.is_open()||!os.is_open()){
+        cout<<"Ошибка открытия файла!"<<endl;
+    }
+    else{
+        cout<<"Файл успешно открыт!"<<endl;
+        int n,l;
+        string str;
+        cout<<"Введите длинну строки n:";
+        cin>>n;
+        while(getline(is, str)){
+            l=str.length();
+            if (l<n){
+                os<<str<<endl;
+            }
+        }
+        cout<<"урааааааааааааа."<<endl;
+        is.close();
+        os.close();
+    }
+}
