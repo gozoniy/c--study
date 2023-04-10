d01a0c506ff16a9f7bd54bbd287a9979c104d485
1gone <soba4ka808@gmail.com>
2023-04-10 15:39:29 +0700

    lab6 fixes

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
-#include "rectangle.h"
+#include "rectangle.cpp"
 
 int main(void){
     system("chcp 1251");
-    rectangle r1;
+
+    rectangle r1;                               //статическая переменная
+    r1.set();
+    r1.print();
+    r1.area();
 
     int l;
-    cout<<"Укажите длинну массива:\n";
+    cout<<"Укажите длинну массива:\n";  
     cin>>l;
-    rectangle *R = new rectangle[l];
+    rectangle *R = new rectangle[l];            //динамическая переменная
     for (int i=0;i<l;i++){
         cout<<"Прямоугольник №"<<i+1<<endl;
         R[i].set();
