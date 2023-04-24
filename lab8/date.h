9526c0181f39058d8aaf0dc7f49d9800af01b02b
1gone <soba4ka808@gmail.com>
2023-04-24 16:11:08 +0700

    lab8 ~~

--- lab8/date.h
+++ lab8/date.h
@@ -7,18 +7,34 @@ public:
     void set();
     void print();
     long format();
+    int days();
+    void undays(int N);
 
     //Конструкторы
     date(){srand(time(NULL)); day=rand()%31+1; month = rand()%12+1; year = rand()%2023+1;}
     date(int d, int m, int y){day = d; month = m; year = y;}
     date(date &A){day = A.day; month = A.month; year = A.year;}
     //Деструктор
-    ~date(){cout<<"Дата "<<day<<"."<<month<<"."<<year<<" удалена.\n";}
+    //~date(){cout<<"Дата "<<day<<"."<<month<<"."<<year<<" удалена.\n";}
 
-    const date operator + (const date &Date) const{
+    //оператор + 
+    const date operator + (date &Date){
+        date curr = *this;
         date Res;
-        Res.day = Date.day + day;
+        Res.undays(curr.days() + Date.days()) ;
         return Res;
+    }
+    //оператор - (количество дней между датами)
+    const int operator - (date &Date) {
+        date curr = *this;
+        int Res;
+        Res = (curr.days() - Date.days()) ;
+        return Res;
+    }
+    date operator ++ () {
+        int N = days();
+        undays(N++);
+        return *this;
     }
 };
 
--- lab8/main.cpp
+++ lab8/main.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 int main(void){
     system("chcp 1251");
-    date d1;                //по умолчанию
+
+    date d1(30,11,2023);                //по умолчанию
     d1.print();
     date d2(12,12,2023);    //с параметрами
     d2.print();
+    cout<<"Дней между d1 и d2: "<<d2 - d1<<endl;
+
     date d3(d2);            //копий
     d3.print();
-    date D = d1 + d2;
-    D.print();
+    ++d3;
+    d3.print();
+    
 }
