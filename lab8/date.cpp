9e39f829c465954baa938f870916be01fb82f188
1gone <soba4ka808@gmail.com>
2023-04-27 02:05:28 +0700

    lab 8 almost complete

--- lab8/date.cpp
+++ lab8/date.cpp
@@ -7,8 +7,8 @@ void date::set(){     //метод ввода дня, месяца и года
     cin>>day>>month>>year;
 }
 void date::print(){   //метод вывода даты ДД.ММ.ГГГГ
-    cout<<"Дата в формате ДД.ММ.ГГГГ: \n"
-    <<day<<"."<<month<<"."<<year<<endl;
+    //cout<<"Дата в формате ДД.ММ.ГГГГ: \n"
+    cout<<day<<"."<<month<<"."<<year<<endl;
 }
 long date::format(){   //метод вывода в формате дней
     long form = year*10000+month*100+day;
@@ -16,19 +16,28 @@ long date::format(){   //метод вывода в формате дней
 }
 int date::days(){      //метод превращения полей класса в количество дней
     int M = 0;
-    for (int i = 0; i<month ; i++){
-        if (i%2 == 0){M+=30;}
+    for (int i = 1; i<month ; i++){//Счёт месяцев
+        if (i == 2){M+=28;}//проверка на февраль
+        else if (i%2 == 0){M+=30;}
         else {M+=31;}
     }
     int Ans = year * 365 + M + day;
     return Ans;
 }
 void date::undays(int N){   //метод превращения количества дней в поля класса
-    year = N%365;
+    year = N/365;
     N = N - year * 365;
-    month = N%30;
-    N = N - month * 30;
+    int M = 0;
+    int k = 1;
+    while (!(N<32)){//Счёт месяцев
+        if (k == 2){M=28;}//проверка на февраль
+        else if (k%2 == 0){M=30;}
+        else {M=31;}
+        k++;
+        N-=M;
+    }
     day = N;
+    
 }
 
 
--- lab8/date.h
+++ lab8/date.h
@@ -11,17 +11,32 @@ public:
     void undays(int N);
 
     //Конструкторы
-    date(){srand(time(NULL)); day=rand()%31+1; month = rand()%12+1; year = rand()%2023+1;}
+    date(){srand(time(NULL)); day=rand()%31+1; month = rand()%12+1; year = 2022+rand()%2;}
     date(int d, int m, int y){day = d; month = m; year = y;}
     date(date &A){day = A.day; month = A.month; year = A.year;}
     //Деструктор
-    //~date(){cout<<"Дата "<<day<<"."<<month<<"."<<year<<" удалена.\n";}
+    ~date(){}
 
-    //оператор + 
-    const date operator + (date &Date){
+//Арифметические операторы
+    const void operator = (date Date){
         date curr = *this;
         date Res;
-        Res.undays(curr.days() + Date.days()) ;
+        undays(Date.days());
+    }
+    //оператор + (Прибавление дней к дате)
+    const date operator + (int Date){
+        date curr = *this;
+        date Res;
+        undays(curr.days() + Date);
+        Res.undays(curr.days() + Date) ;
+        return Res;
+    }
+    //оператор - (Вычитание дней из даты)
+    const date operator - (int Date){
+        date curr = *this;
+        date Res;
+        undays(curr.days() - Date);
+        Res.undays(curr.days() - Date) ;
         return Res;
     }
     //оператор - (количество дней между датами)
@@ -31,11 +46,38 @@ public:
         Res = (curr.days() - Date.days()) ;
         return Res;
     }
-    date operator ++ () {
+
+//Унарные операторы
+    //оператор ++ (Завтра)
+    const date operator ++ () {
         int N = days();
-        undays(N++);
+        undays(++N);
         return *this;
     }
+    //оператор -- (Вчера)
+    const date operator -- () {
+        int N = days();
+        undays(--N);
+        return *this;
+    }
+
+//Операторы отношения
+    const bool operator == (date &Date){
+        date curr = *this;
+        if (curr.days()==Date.days()){return true;}
+        else {return false;}
+    }
+        const bool operator > (date &Date){
+        date curr = *this;
+        if (curr.days()>Date.days()){return true;}
+        else {return false;}
+    }
+    const bool operator < (date &Date){
+        date curr = *this;
+        if (curr.days()<Date.days()){return true;}
+        else {return false;}
+    }
+
 };
 
 #endif DATE_H
