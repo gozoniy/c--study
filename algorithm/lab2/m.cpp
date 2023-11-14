a9dfe2bf02d0dcd8bde375ecc5938de392824db3
GoZone <soba4ka808@gmail.com>
2023-11-15 01:47:55 +0700

    lab 2 mergeS complete

--- algorithm/lab2/m.cpp
+++ algorithm/lab2/m.cpp
@@ -13,6 +13,7 @@ void getFile(string filename, int size, int n1, int n2){
     out.close();
 }
 
+//Сортировка прямым слиянием
 void mergeS(string filename,int size){
     //Выполняем операцию log2(size) раз
     for (int i = 1; i<size; i*= 2){
@@ -77,50 +78,64 @@ void mergeS(string filename,int size){
     //remove("buffer2.txt");
 }
 
+//Трёхпутевая сортировка многопутевым слиянием
 void balanceMergeSort(string filename,int size){
     //Выполняем операцию log2(size) раз
     for (int i = 1; i<size; i*= 2){
         ifstream in(filename);
-        ofstream out1("buffer1.txt");
-        ofstream out2("buffer2.txt");
-        //Деление в два временных файла через один элемент
+        //Деление исходного файла на три временных поровну
         int c = 0;
         int temp = 0;
         in>>temp;
-        while(!in.eof()){
-            if (c>(size/2))
-                out1<<temp<<" ";
-            else
-                out2<<temp<<" ";
-            in>>temp;
-            c++;
+        int tapes = 3;
+        for (int i = 0; i<tapes; i++){
+            ofstream out;
+            out.open("buffer"+to_string(i+1)+".txt");
+            int start = (size / tapes) *i;
+            int end = (size / tapes) * (i + 1);
+            while (start <= c && c<end){
+                out<<temp<<" ";
+                c++;
+                in>>temp;
+            }
+            out.close();
         }
+        
         in.close();
-        out1.close();
-        out2.close();
-        int size1, size2;
-        size1 = size/2;
-        size2 = size - size1;
+
+    
+        
+        int size1, size2, size3;
+        size1 = size/3;
+        size2 = (size/3)*2;
+        size3 = (size/3)*3;
         //Слияние
         ofstream os(filename);
         ifstream in1("buffer1.txt");
         ifstream in2("buffer2.txt");
+        ifstream in3("buffer3.txt");
         int t1;
-        int o, p;
-        o = p = 0;
-        int temp1, temp2;
+        int o, p, m;
+        o = p = m = 0;
+        int temp1, temp2, temp3;
         in1>>temp1;
         in2>>temp2;
+        in3>>temp3;
         for (int i = 0; i < size; i++){
-            if (p>=(size2)||(o<size1 && temp1<temp2)){
+            if (o<size1 && temp1<temp2 && temp1<temp3){
                 os<<temp1<<" ";
                 o++;
                 in1>>temp1;
             }
-            else{
+            else if (p<size2 && temp2<temp1 && temp2<<temp3){
                 os<<temp2<<" ";
                 p++;
                 in2>>temp2;
+            }
+            else if (m<size3 && temp3<temp2 && temp3<<temp1){
+                os<<temp3<<" ";
+                m++;
+                in3>>temp3;
             }
         }
         os.close();
@@ -134,9 +149,9 @@ void balanceMergeSort(string filename,int size){
 
 int main(void){
     system("chcp 1251");
-    getFile("numbers.txt",10000,1,5);
+    getFile("numbers.txt",100000,1,5);
     int t_1 = clock();
-    balanceMergeSort("numbers.txt",10000);
+    mergeS("numbers.txt",100000);
     int t_2 = clock();
     int ans = t_2 - t_1;
     cout<<" эл отсортированы за "<<ans<<"ms\n";
